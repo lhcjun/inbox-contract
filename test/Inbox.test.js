acfa5469c57805e25968058f027cc2510df2d4f2
Jun <lhcjun@gmail.com>
2020-11-15 23:31:54 +0800

    fetch accounts from Ganache

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -4,3 +4,17 @@ const Web3 = require('web3'); // Web3 constructor (Eth JS API)
 
 // create instance of web3 library & connect to ganache test network with provider
 const web3 = new Web3(ganache.provider());
+
+let accounts;
+
+beforeEach(async () => {
+  // get a list of all accounts from ganache
+  accounts = await web3.eth.getAccounts();
+  // use one of those accounts to deploy the contract
+});
+
+describe('Inbox', () => {
+  it('deploys a contract', () => {
+    console.log(accounts);
+  });
+});
