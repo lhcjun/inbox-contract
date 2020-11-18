834f45a957c964ec8d3ca502ce4d3abfa9a15802
Jun <lhcjun@gmail.com>
2020-11-18 20:11:48 +0800

    test initial message variable in constructor

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -8,18 +8,25 @@ const web3 = new Web3(ganache.provider());
 
 let accounts;
 let inbox;
+const initialMsg = 'Hi there!';
 
 beforeEach(async () => {
   // get a list of all accounts from ganache
   accounts = await web3.eth.getAccounts();
   // use one of those accounts to deploy the contract (need abi & bytecode to create a contract)
   inbox = await new web3.eth.Contract(abi) // interact with existed contract / deploy new contract
-    .deploy({ data: '0x' + evm.bytecode.object, arguments: ['Hi there!'] })
+    .deploy({ data: '0x' + evm.bytecode.object, arguments: [initialMsg] })
     .send({ from: accounts[0], gas: '1000000' }); // send transaction
 });
 
 describe('Inbox', () => {
   it('deploys a contract in beforeEach', () => {
     assert.ok(inbox.options.address);
+  });
+
+  it('sends a default msg to the new instance of constructor when deploying the contract', async () => {
+    // get message variable from the deployed contract
+    const msg = await inbox.methods.message().call(); // invoke the contract function by 'call'
+    assert.equal(msg, initialMsg);
   });
 });
